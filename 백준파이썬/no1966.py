8862bb6060d291f66e26900ea81d03248dac2d6f
leekihoon <leekihoon1689@gmail.com>
2021-07-27 19:12:01 +0900

    210727

---
+++ 백준파이썬/no1966.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for _ in range(t) :
+    n, m = map(int,sys.stdin.readline().split())
+    array = deque()
+    array = int(sys.stdin.readline().split(''))
+    print(array)
+        
