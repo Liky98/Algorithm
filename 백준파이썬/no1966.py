96c27ca66854796ee7db3dca3a911c6f5c33a7dd
leekihoon <leekihoon1689@gmail.com>
2021-07-29 13:55:14 +0900

    210729

--- 백준파이썬/no1966.py
+++ 백준파이썬/no1966.py
@@ -1,11 +1,38 @@
 import sys
 from collections import deque
 
+
 t = int(sys.stdin.readline())
 
 for _ in range(t) :
     n, m = map(int,sys.stdin.readline().split())
-    array = deque()
-    array = int(sys.stdin.readline().split(''))
-    print(array)
-        
+    #array = deque()
+    array = deque(list(map(int, sys.stdin.readline().split())))
+    index = deque(list(range(len(array))))
+
+    index[m] = -1 #타겟을 -1로 설정
+    count= 0
+    
+    while True:
+        if array[0] != max(array) :
+            array.append(array[0])
+            array.popleft()
+            index.append(index[0])
+            index.popleft()
+            
+        else :
+            count += 1
+            if index[0] == -1 :
+                print(count)
+                break
+            else :
+                array.popleft()
+                index.popleft()
+                
+            
+            
+    
+    
+    
+    
+    
